5f824c5de06b27237c7034a4f5e8874593fd82a7
M_Imran <mimranjafar8@gmail.com>
2025-02-06 10:20:18 +0500

    plan fixes

--- src/app/api/users/profile/[id]/route.tsx
+++ src/app/api/users/profile/[id]/route.tsx
@@ -4,6 +4,7 @@ import Profile from '../../../../../../mongoose/models/profile-model';
 import User from "../../../../../../mongoose/models/user-model";
 import { verifyToken } from "../../../../lib/jwt";
 import sendMail from "@/app/lib/email";
+import Subscription from "../../../../../../mongoose/models/subscription-model";
 
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 export async function PUT(req: any) {
@@ -26,7 +27,7 @@ export async function PUT(req: any) {
     if (!user) {
       return new Response(JSON.stringify({ msg: "User not found" }), { status: 404 });
     }
-    
+
     if (name) user.name = name;
     if (email) user.email = email;
     if (phone_number) user.phone_number = phone_number;
@@ -37,12 +38,12 @@ export async function PUT(req: any) {
     if (!profile) {
       return new Response(JSON.stringify({ msg: "Profile not found" }), { status: 404 });
     }
-    
+
     if (profile_img.name) profile.profile_img = imageUrl;
     if (address) profile.address = address;
     if (dob) profile.dob = dob;
     if (org_name) profile.org_name = org_name;
-    
+
     await profile.save();
     await sendMail(receiver_email, email_subject, email_body)
     return new Response(
@@ -80,17 +81,25 @@ export async function GET(req: any) {
         path: "profile",
         strictPopulate: true,
       })
-      // .populate({
-      //   path: "subscription",
-      //   strictPopulate: true,
-      // })
+    // .populate({
+    //   path: "subscription",
+    //   strictPopulate: true,
+    // })
+    const subscriptions = await Subscription.findOne({ user_id: data._id });
+    if (!subscriptions) {
+      throw new Error("Subscription not found for the given user.");
+    }
+    const activePlan = subscriptions.subscription.find((plan:any) => plan.status === "active");
     if (!data) {
       return new Response(JSON.stringify({ msg: "Profile not found" }), { status: 404 });
     }
     const { password, ...userData } = data._doc;
-    return new Response(JSON.stringify({ data: {name:userData.name, email:userData.email, phone_number:userData.phone_number, verified:userData.verified,
-       profile_img:userData.profile.profile_img, org_name:userData.profile.org_name, subscriptions:userData.subscription.subscription
-      }, msg: "success" }), { status: 200 });
+    return new Response(JSON.stringify({
+      data: {
+        name: userData.name, email: userData.email, phone_number: userData.phone_number, verified: userData.verified,
+        profile_img: userData.profile.profile_img, org_name: userData.profile.org_name, plan: activePlan
+      }, msg: "success"
+    }), { status: 200 });
   } catch (error: any) {
     console.error("Error getting profile:", error);
     return new Response(JSON.stringify({ error: error.message }), { status: 400 });
